b955731e7fae9241bd4275c647a57b319b429546
Takey Tajwar <tajwar@senex.local>
2023-01-02 21:55:52 +0600

    update score after foul

--- scripts/Score_Board.gd
+++ scripts/Score_Board.gd
@@ -4,8 +4,6 @@ extends Label
 var score_A = 0
 var score_B = 0
 
-var last_scorer = null
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,22 +21,20 @@ func update_score():
 func _on_Ball_goal(name):
 	if name == 'Goal_A':
 		score_B += 1
-		last_scorer = name
+		update_score()
 	elif name == 'Goal_B':
 		score_A += 1
-		last_scorer = name
+		update_score()
 	else:
 		push_warning("invalid goal name!")
-	update_score()
 
 
 func _on_Ball_foul(last_pusher):
-	print(last_pusher)
 	if last_pusher == 'Player_A':
 		score_B += 1
-		last_scorer = name
+		update_score()
 	elif last_pusher == 'Player_B':
 		score_A += 1
-		last_scorer = name
+		update_score()
 	else:
 		push_warning("invalid last pusher name!")
