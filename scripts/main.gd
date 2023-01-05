7c32668d60103e550e99a9a10ac333c35acd61fe
Takey Tajwar <tajwar@senex.local>
2023-01-05 21:19:56 +0600

    add game data checking and saving

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,24 +1,63 @@
 extends Spatial
 
+onready var ball = get_node("Ball")
+onready var player_a = get_node("Player_A")
+onready var player_b = get_node("Player_B")
+onready var sboard = get_node("Score_Board")
 
-var score_A = 0
-var score_B = 0
+const DATA_PATH = "res://data.json"
+
+var data = []
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	read_data()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Ball_goal(name):
-	if name == 'Goal_A':
-		score_B += 1
-	elif name == 'Goal_B':
-		score_A += 1
+func read_data():
+	var file = File.new()
+	if file.file_exists(DATA_PATH):
+		print("Data file exists")
+		file.open(DATA_PATH, File.READ)
+		data = parse_json(file.get_as_text())
+		file.close()
 	else:
-		push_warning("invalid goal name!")
+		print("Data file doesn't exist")
+		data = []
+
+
+
+func save_data():
+	var file = File.new()
+	file.open(DATA_PATH, File.WRITE)
+	file.store_line(to_json(data))
+	file.close()
+
+
+func add_data(new_data):
+	data.append(new_data)
+
+func _on_Timer_Get_Data_timeout():
+	var new_data = {
+		## ball X, Z
+		"B_X": ball.transform.origin.x,
+		"B_Z": ball.transform.origin.z,
+		## Players X, Z
+		"PA_X": player_a.transform.origin.x,
+		"PA_Z": player_a.transform.origin.z,
+		"PB_X": player_b.transform.origin.x,
+		"PB_Z": player_b.transform.origin.z,
+		## Score
+		"SA": sboard.get("score_A"),
+		"SB": sboard.get("score_B"),
+		## Ball Velocity
+		"B_V_X": ball.linear_velocity.x,
+		"B_V_Z": ball.linear_velocity.z,
+	}
+	
+	print(new_data)
+	add_data(new_data)
+
+
+func _on_Timer_Save_Data_timeout():
+	save_data()
